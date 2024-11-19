f24d90c4c673079308f136d5748d82a4ee27cb4e
Joel_Contreras <163866701+JoelContrerasT@users.noreply.github.com>
2024-11-19 11:45:17 -0500

    Create holaMundo.cs

---
+++ holaMundo.cs
@@ -0,0 +1,17 @@
+// Importa el namespace System, que contiene funcionalidades esenciales como la clase Console
+using System;
+
+// Define una clase llamada Program, que será el contenedor principal de tu código
+class Program
+{
+    // Este es el punto de entrada del programa: el método Main
+    // Es el primer método que se ejecuta cuando inicias el programa
+    static void Main(string[] args)
+    {
+        // Escribe un mensaje en la consola
+        Console.WriteLine("Hola, Mundo");
+
+        // Tarea practica
+        Console.WriteLine("Hola, soy Joel Contreras. ¡Estoy aprendiendo C#!");
+    }
+}
